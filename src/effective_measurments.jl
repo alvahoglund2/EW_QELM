822cc289dd2c2853d04f1e7fa32e9d69d27a0cd7
alvahoglund2 <hoglundalva@gmail.com>
2025-03-04 13:36:15 +0100

    Fix bug in effective measurments

--- src/effective_measurments.jl
+++ src/effective_measurments.jl
@@ -1,6 +1,6 @@
 function get_eff_measurment(measurment_op :: AbstractMatrix, ρ_r :: AbstractMatrix,
     hamiltonian :: AbstractMatrix, t_ev, 
-    d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis, 
+    d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis; 
     truncate_density_matrix :: Bool = true,) 
 
     op_ev = operator_evolution(measurment_op, t_ev, hamiltonian)
@@ -8,7 +8,7 @@ function get_eff_measurment(measurment_op :: AbstractMatrix, ρ_r :: AbstractMat
     exp_value(ρ_I) = tr(wedge([ρ_I, ρ_r], [d_main, d_res], d)*op_ev)
 
     function f_vec_trunc(ρ_I_vec_trunc)
-        ρ_I_trunc = reshape(ρ_I_vec_trunc, 4, 4)
+        ρ_I_trunc = reshape(ρ_I_vec_trunc, 4, 4)'
         ρ_I = zeros(Complex{Float64}, 16, 16)
         ρ_I[get_qubit_idx(),get_qubit_idx()] = ρ_I_trunc
         return exp_value(ρ_I)
@@ -19,13 +19,14 @@ function get_eff_measurment(measurment_op :: AbstractMatrix, ρ_r :: AbstractMat
         return exp_value(ρ_I)
     end
 
-
-    lmap = if truncate_density_matrix
+    n_eff = nothing
+    if truncate_density_matrix
         lmap = LinearMap(f_vec_trunc, 1, 16)
+        n_eff = sparse(reshape(Matrix{Complex{Float64}}(lmap), 4, 4))
     else
         lmap = LinearMap(f_vec, 1, 16^2)
+        n_eff = sparse(reshape(Matrix{Complex{Float64}}(lmap), 16, 16))
     end
 
-    n_eff_trunc = sparse(reshape(Matrix{Complex{Float64}}(lmap), 4, 4))
-    return n_eff_trunc
+    return n_eff
 end
