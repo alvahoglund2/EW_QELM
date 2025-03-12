e3edc11d07768ad9b87928a130cee1c8b084dae1
alvahoglund2 <hoglundalva@gmail.com>
2025-03-12 13:31:02 +0100

    Add functionality to calculate effective measurement and time evolution for specified focknumber

--- src/effective_measurments.jl
+++ src/effective_measurments.jl
@@ -30,3 +30,50 @@ function get_eff_measurment(measurment_op :: AbstractMatrix, ρ_r :: AbstractMat
 
     return n_eff
 end
+
+
+function get_eff_measurment(op_ev :: AbstractMatrix, ρ_r :: AbstractMatrix,
+    d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis; )
+    """
+        get_eff_measurment(op_ev :: AbstractMatrix, ρ_r :: AbstractMatrix,
+        d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis; )
+
+    Returns the effective measurement for the given evolved measurment operator.
+    """
+    exp_value(ρ_I) = tr(wedge([sparse(ρ_I), ρ_r], [d_main, d_res], d)*op_ev)
+
+    function f_vec_trunc(ρ_I_vec_trunc)
+        ρ_I_trunc = reshape(ρ_I_vec_trunc, 4, 4)'
+        ρ_I = zeros(Complex{Float64}, 16, 16)
+        ρ_I[get_two_qubit_idx(d_main),get_two_qubit_idx(d_main)] = ρ_I_trunc
+        return exp_value(ρ_I)
+    end
+
+    lmap = LinearMaps.LinearMap(f_vec_trunc, 1, 16)
+    n_eff = sparse(reshape(Matrix{Complex{Float64}}(lmap), 4, 4))
+    return n_eff
+
+end
+
+function get_eff_measurments(measurment_ops, ρ_r :: AbstractMatrix,
+    hamiltonian :: AbstractMatrix, t_ev, 
+    d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis, 
+    focknbrs)
+    """
+        get_eff_measurments(measurment_ops, ρ_r :: AbstractMatrix,
+        hamiltonian :: AbstractMatrix, t_ev, 
+        d :: FermionBasis, d_main :: FermionBasis, d_res :: FermionBasis; 
+        focknbrs)
+
+    Returns the effective measurements for the given measurment operators 
+    and specified focknbrs.
+    """
+    u_block = get_propagator_block(t_eval, hamiltonian, d, focknbrs)
+    eff_measurments = []
+    for op in measurment_ops
+        op_ev = operator_evolution_blocks(op, u_block, d, focknbrs)
+        push!(eff_measurments, get_eff_measurment(op_ev, ρ_r, d, d_main, d_res))
+    end
+    return eff_measurments
+
+end
--- src/time_evolution.jl
+++ src/time_evolution.jl
@@ -19,7 +19,7 @@ function operator_evolution(M:: AbstractMatrix, t_end :: AbstractFloat, hamilton
     Returns the time evolved operator.
     """
     Hd = Matrix(hamiltonian)
-    u = sparse(exp(-im * t_end * Hd))
+    u = sparse(cis(-t_end * Hd))
     Mt = u' * M * u
     return Mt
 end
@@ -41,4 +41,37 @@ function state_time_ev(ρ0 :: AbstractMatrix, t_span ::Tuple, hamiltonian :: Abs
     prob = ODEProblem(ρ_ode, ρ0, t_span, params, reltol=1e-12, abstol=1e-12)
     ρt = solve(prob, Tsit5())
     return ρt
+end
+
+ 
+function get_propagator_block(t_eval :: AbstractFloat, hamiltonian:: AbstractMatrix, d:: FermionBasis, focknbrs)
+    """
+        get_propagator_block(t_eval :: AbstractFloat, hamiltonian:: AbstractMatrix, d:: FermionBasis, focknbrs)
+
+    Return a vector of propagator blocks for the specified forcknumbers 
+    """
+    u_blocks = []
+    for fn in focknbrs
+        ind = d.symmetry.qntoinds[fn]
+        h_block = hamiltonian[ind,ind] |> Matrix
+        u_block = cis(-t_eval * h_block)
+        push!(u_blocks, sparse(u_block))
+    end
+    return u_blocks
+end
+
+function operator_evolution_blocks(M:: AbstractMatrix, u_blocks, d:: FermionBasis, focknbrs)
+    """
+        operator_evolution_blocks(M:: AbstractMatrix, u_blocks, d:: FermionBasis, focknbrs)
+    
+    Return the time evolved operator. Only evolves the blocks corresponding to the focknbrs and u_blocks.
+    """
+    Mt = spzeros(ComplexF64, get_basis_dim(d), get_basis_dim(d))
+    for (i, fn) in enumerate(focknbrs)
+        ind = d.symmetry.qntoinds[fn]
+        M_block = M[ind,ind] 
+        Mt_block = u_blocks[i]' * M_block * u_blocks[i]
+        Mt[ind,ind] = Mt_block
+    end
+    return Mt
 end
