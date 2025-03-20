451b23519025a0dae54af0367c9a9fc5d9a4ace2
alvahoglund2 <hoglundalva@gmail.com>
2025-03-20 15:02:35 +0100

    Add evolution based on fock_nbr to compare_states

--- Compare_methods/compare_methods.jl
+++ Compare_methods/compare_methods.jl
@@ -42,6 +42,19 @@ function measure_states(state_list, measurment_list)
     return result
 end
 
+function measure_states(state_list, eff_measurments, d_main)
+    n_states = length(state_list)
+    n_measurements = length(eff_measurments)
+    result = zeros(n_states, n_measurements)
+    for (i, state) in enumerate(state_list)
+        for (j, eff_measurment) in enumerate(eff_measurments)
+            trunc_state = state[get_qubit_idx(d_main),get_qubit_idx(d_main)]
+            result[i, j] = expectation_value(trunc_state, eff_measurment)
+        end
+    end    
+    return result
+end
+
 ## ------------ Evolve states ----------------
 evolved_states = [state_evolution(state, t_eval, hamiltonian) for state in states_tot]
 measurments_schrödinger = measure_states(evolved_states, ops)
@@ -55,7 +68,15 @@ eff_measurments = [get_eff_measurment(op, ρ_R, hamiltonian, t_eval, d, d_main, 
 states_trunc = [state[get_qubit_idx(d_main),get_qubit_idx(d_main)] for state in states]
 measurments_effective = measure_states(states_trunc, eff_measurments)
 
+## ------------ Evolve only specified sector ----------------
+
+fock_nbrs = 2+qn
+ops = vcat([nbr_op(i, d) for i in 1:nbr_dots], [nbr2_op(i, d) for i in 1:nbr_dots])
+eff_measurments = get_eff_measurments(ops, ρ_R, hamiltonian, t_eval, d, d_main, d_res, fock_nbrs)
+measurment_focknbr = measure_states(states, eff_measurments, d_main)
+
 ## ------------ Compare ----------------
 println(measurments_heisenberg  ≈ measurments_schrödinger)
 println(measurments_effective ≈ measurments_schrödinger)
 println(measurments_effective ≈ measurments_heisenberg)
+println(measurment_focknbr ≈ measurments_effective)
